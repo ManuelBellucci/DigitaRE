dd70b0929869187442ed99592e123a4cef37c6ea
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-27 16:35:51 +0200

    created second section data json

--- src/Components/SecondSection/SecondSection.jsx
+++ src/Components/SecondSection/SecondSection.jsx
@@ -1,17 +1,25 @@
 import CTAButton from '../commons/CTAButton'
+import SecondSectionContent from './utils/SecondSectionContent'
+import data from '../../data/secondsection.json'
 
 const SecondSection = () => {
   return (
     <section className='w-full h-[596px] shrink-0 grid grid-cols-2'>
       <div className='flex self-center justify-center'>
-        <img src='/src/assets/secondsection.svg' alt='second section image' />
+        <img
+          src={data.image.src}
+          alt={data.image.alt}
+        />
       </div>
       <div className='flex flex-col self-center w-[523px] items-start gap-8'>
-        <div className='flex flex-col gap-5'>
-          <h2 className='self-stretch text-4xl font-bold'>Lorem ipsum</h2>
-          <p className='text-myGray'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut, perspiciatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi illo quaerat tenetur error dignissimos.</p>
-        </div>
-        <CTAButton href='/' text='text' />
+        <SecondSectionContent
+          title={data.title}
+          description={data.description}
+        />
+        <CTAButton
+          href={data.button.href}
+          text={data.button.text}
+        />
       </div>
     </section>
   )
