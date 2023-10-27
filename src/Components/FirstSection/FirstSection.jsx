a11046567ea6e3dbf9f74de5b8c790e5e3700d51
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-27 21:20:38 +0200

    destructuring props  and mapped cards 1st section

--- src/Components/FirstSection/FirstSection.jsx
+++ src/Components/FirstSection/FirstSection.jsx
@@ -5,52 +5,40 @@ import FirstSectionHeader from './utils/FirstSectionHeader'
 import data from '../../data/firstsection.json'
 
 const FirstSection = () => {
+  const { title, description } = data.header
+  const { first, second, third, fourth, fifth, sixth } = data.cards
+
+  const cardData = [first, second, third]
+  const cardData2 = [fourth, fifth, sixth]
+
   return (
     <section className='bg-[#110F0F] flex w-full px-[70px] pt-20 pb-[70px] justify-center items-center'>
       <FirstSectionContainer>
         <FirstSectionHeader
-          title={data.header.title}
-          description={data.header.description}
+          title={title}
+          description={description}
         />
         <CardContainer>
-          <Card
-            alt={data.cards.first.alt}
-            svg={data.cards.first.svg}
-            title={data.cards.first.title}
-            description={data.cards.first.description}
-          />
-          <Card
-            alt={data.cards.second.alt}
-            svg={data.cards.second.svg}
-            title={data.cards.second.title}
-            description={data.cards.second.description}
-          />
-          <Card
-            alt={data.cards.third.alt}
-            svg={data.cards.third.svg}
-            title={data.cards.third.title}
-            description={data.cards.third.description}
-          />
+          {cardData.map((card, index) => (
+            <Card
+              key={index}
+              alt={card.alt}
+              svg={card.svg}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
         </CardContainer>
         <CardContainer>
-          <Card
-            alt={data.cards.fourth.alt}
-            svg={data.cards.fourth.svg}
-            title={data.cards.fourth.title}
-            description={data.cards.fourth.description}
-          />
-          <Card
-            alt={data.cards.fifth.alt}
-            svg={data.cards.fifth.svg}
-            title={data.cards.fifth.title}
-            description={data.cards.fifth.description}
-          />
-          <Card
-            alt={data.cards.sixth.alt}
-            svg={data.cards.sixth.svg}
-            title={data.cards.sixth.title}
-            description={data.cards.sixth.description}
-          />
+          {cardData2.map((card, index) => (
+            <Card
+              key={index}
+              alt={card.alt}
+              svg={card.svg}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
         </CardContainer>
       </FirstSectionContainer>
     </section>
