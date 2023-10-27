35cc83b244e93c6ba0a78c8e6ef01dc06f0fe6d5
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-27 16:21:57 +0200

    created SecondSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Hero from './Components/Hero/Hero.jsx'
 import FirstSection from './Components/FirstSection/FirstSection'
+import SecondSection from './Components/SecondSection/SecondSection'
+
 function App () {
   return (
     <>
       <Hero />
       <FirstSection />
+      <SecondSection />
     </>
   )
 }
