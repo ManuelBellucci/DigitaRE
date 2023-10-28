5cbfa859fe79a64fe8ecd5b4b483f925a55e62da
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-28 10:54:15 +0200

    responsive changes for 2nd section

--- src/Components/SecondSection/SecondSection.jsx
+++ src/Components/SecondSection/SecondSection.jsx
@@ -4,15 +4,15 @@ import data from '../../data/secondsection.json'
 
 const SecondSection = () => {
   return (
-    <section className='bg-black w-full h-[596px] shrink-0 grid grid-cols-2'>
+    <section className='bg-black w-full h-[596px] shrink-0 grid grid-cols-1 md:grid-cols-2'>
       <div className='flex self-center justify-center'>
         <img
           src={data.image.src}
           alt={data.image.alt}
-          className='max-h-96'
+          className='max-h-56 md:max-h-72 lg:max-h-96'
         />
       </div>
-      <div className='flex flex-col self-center w-[523px] items-start gap-8'>
+      <div className='flex flex-col self-center lg:w-[70%] items-start gap-4 md:gap-6 lg:gap-8 px-6 md:px-0'>
         <SecondSectionContent
           title={data.title}
           description={data.description}
--- src/Components/SecondSection/utils/SecondSectionContent.jsx
+++ src/Components/SecondSection/utils/SecondSectionContent.jsx
@@ -1,8 +1,8 @@
 const SecondSectionContent = ({ title, description }) => {
   return (
-    <div className='flex flex-col gap-5'>
+    <div className='flex flex-col gap-3 md:gap-4 lg:gap-5'>
       <h2 className='self-stretch text-4xl font-bold'>{title}</h2>
-      <p className='text-myGray'>{description}</p>
+      <p className='text-myGray text-sm md:text-base'>{description}</p>
     </div>
   )
 }
