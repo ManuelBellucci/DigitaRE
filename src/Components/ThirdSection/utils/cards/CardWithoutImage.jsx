ad3ed42b78150ca0c14de27d9648f0c1272fd04c
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-28 15:38:32 +0200

    responsive text for card without image

--- src/Components/ThirdSection/utils/cards/CardWithoutImage.jsx
+++ src/Components/ThirdSection/utils/cards/CardWithoutImage.jsx
@@ -4,7 +4,7 @@ const CardWithoutImage = ({ tagText, title, href }) => {
   return (
     <div className='flex flex-col items-start p-2 gap-2 md:p-4 md:gap-4 lg:gap-5 lg:p-6 border border-white rounded w-full hover:bg-myGray transition-all ease-in hover:text-black group '>
       <Tag tagText={tagText} />
-      <h3 className='text-2xl font-bold'>{title}</h3>
+      <h3 className='text-base md:text-xl lg:text-2xl font-bold'>{title}</h3>
       <Readmore href={href} />
     </div>
   )
