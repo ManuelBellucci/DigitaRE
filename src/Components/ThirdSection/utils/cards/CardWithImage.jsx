ecefb95abbf35adf03585a191a33177c2fce38bb
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-28 15:38:20 +0200

    added w-full to image

--- src/Components/ThirdSection/utils/cards/CardWithImage.jsx
+++ src/Components/ThirdSection/utils/cards/CardWithImage.jsx
@@ -5,7 +5,7 @@ const CardWithImage = ({ tagText, title, src, href }) => {
   return (
     <div className='flex flex-col p-2 md:p-4 lg:p-6 items-start gap-2 md:gap-4 lg:gap-6 border border-white rounded w-full hover:bg-myGray transition-all ease-in hover:text-black group'>
 
-      <img src={src} alt={src} className='self-center object-cover rounded' />
+      <img src={src} alt={src} className='self-center object-cover rounded w-full' />
 
       <div className='flex flex-col items-start gap-2 md:gap-3 lg:gap-5 py-3 lg:py-6'>
         <Tag tagText={tagText} />
