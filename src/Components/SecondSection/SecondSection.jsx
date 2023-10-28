b2306457545dd04bc0234cffa69acc4f45988128
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-28 15:45:23 +0200

    more space on mobile by adding different sec height

--- src/Components/SecondSection/SecondSection.jsx
+++ src/Components/SecondSection/SecondSection.jsx
@@ -4,7 +4,7 @@ import data from '../../data/secondsection.json'
 
 const SecondSection = () => {
   return (
-    <section className='bg-black w-full h-[596px] shrink-0 grid grid-cols-1 md:grid-cols-2'>
+    <section className='bg-black w-full h-[680px] md:h-[596px] shrink-0 grid grid-cols-1 md:grid-cols-2'>
       <div className='flex self-center justify-center'>
         <img
           src={data.image.src}
