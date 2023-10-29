7dec19317738ff1312b663b76c11030a6d439c4b
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-29 22:07:44 +0100

    responsive adjustments

--- src/Components/SecondSection/utils/SecondSectionContent.jsx
+++ src/Components/SecondSection/utils/SecondSectionContent.jsx
@@ -1,8 +1,8 @@
 const SecondSectionContent = ({ title, description }) => {
   return (
-    <div className='flex flex-col gap-3 md:gap-4 lg:gap-5'>
-      <h2 className='self-stretch text-4xl font-bold'>{title}</h2>
-      <p className='text-myGray text-sm md:text-base'>{description}</p>
+    <div className='flex flex-col gap-2 md:gap-4 lg:gap-5'>
+      <h2 className='self-stretch text-lg lg:text-2xl font-bold'>{title}</h2>
+      <p className='text-myGray text-xs md:text-base'>{description}</p>
     </div>
   )
 }
