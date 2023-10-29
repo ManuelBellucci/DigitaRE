fa9f34613936e61ffa393c0fdcc993edefe6bede
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-29 22:06:33 +0100

    font responsive

--- src/Components/FirstSection/utils/FirstSectionHeader.jsx
+++ src/Components/FirstSection/utils/FirstSectionHeader.jsx
@@ -1,7 +1,7 @@
 const FirstSectionHeader = ({ title, description }) => {
   return (
     <div className='flex flex-col items-start gap-6 self-stretch'>
-      <h2 className='self-stretch text-center text-[40px] font-bold'>
+      <h2 className='self-stretch text-center text-3xl lg:text-[40px] font-bold'>
         {title}
       </h2>
       <p className='flex-1 text-myGray text-center'>
