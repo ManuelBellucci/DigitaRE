2ee01790de8b2e49e32494c279c25737b03c1d5d
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-02 13:10:47 +0100

    modularized

--- src/Components/SecondSection/SecondSection.jsx
+++ src/Components/SecondSection/SecondSection.jsx
@@ -1,29 +1,19 @@
-import CTAButton from '../commons/CTAButton'
-import SecondSectionContent from './utils/SecondSectionContent'
+import SecondSectionContentWrapper from './utils/SecondSectionContentWrapper'
+import SecondSectionImage from './utils/SecondSectionImage'
 import data from '../../data/secondsection.json'
 import { Slide } from 'react-awesome-reveal'
-const SecondSection = () => {
-  return (
-    <section className='bg-black w-full h-min grid grid-cols-2'>
-      <div className='flex justify-center items-center h-[600px] md:h-[700px]'>
-        <Slide triggerOnce>
-          <div className='bg-[url(/assets/persona.png)] bg-contain p-20 md:p-28 lg:p-52 border border-white rounded-full bg-no-repeat' />
-        </Slide>
-      </div>
-      <Slide triggerOnce direction='right' className='flex self-center justify-center'>
-        <div className='flex flex-col self-center lg:w-[70%] items-start gap-8 md:gap-8 lg:gap-9 px-6 md:px-0'>
-          <SecondSectionContent
-            title={data.title}
-            description={data.description}
-          />
-          <CTAButton
-            href={data.button.href}
-            text={data.button.text}
-          />
-        </div>
-      </Slide>
-    </section>
-  )
-}
+
+const SecondSection = () => (
+  <section className='bg-black w-full h-min grid grid-rows-1 lg:grid-cols-2'>
+    <SecondSectionImage />
+    <Slide triggerOnce direction='right' className='flex self-center justify-center'>
+      <SecondSectionContentWrapper
+        title={data.title}
+        description={data.description}
+        button={data.button}
+      />
+    </Slide>
+  </section>
+)
 
 export default SecondSection
--- src/Components/ThirdSection/utils/cards/CardWithImage.jsx
+++ src/Components/ThirdSection/utils/cards/CardWithImage.jsx
@@ -1,19 +1,11 @@
-import Readmore from './Readmore'
-import Tag from './Tag'
+import ImageContainer from './ImageContainer'
+import CardContent from './CardContent'
 
 const CardWithImage = ({ tagText, title, src, href }) => {
   return (
     <div className='flex flex-col p-4 md:p-6 lg:p-9 items-start gap-2 md:gap-4 lg:gap-6 border border-white rounded w-full hover:bg-myGray transition-all ease-in hover:text-black group'>
-
-      <img loading='lazy' src={src} alt={src} className='self-center object-cover rounded w-full' />
-
-      <div className='flex flex-col items-start gap-2 md:gap-3 lg:gap-5 py-3 lg:py-6'>
-        <Tag tagText={tagText} />
-        <h3 className='text-base md:text-xl lg:text-2xl font-bold'>{title}</h3>
-        <div className='flex gap-2 items-center'>
-          <Readmore href={href} />
-        </div>
-      </div>
+      <ImageContainer src={src} />
+      <CardContent tagText={tagText} title={title} href={href} />
     </div>
   )
 }
