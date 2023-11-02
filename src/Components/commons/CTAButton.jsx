89e996e5a82b51e9b46ad889c8118bacfdf4f637
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-02 13:04:25 +0100

    added onClick and className props + aria-label

--- src/Components/commons/CTAButton.jsx
+++ src/Components/commons/CTAButton.jsx
@@ -1,6 +1,11 @@
-const CTAButton = ({ href, text }) => {
+const CTAButton = ({ href, text, className, onClick }) => {
   return (
-    <a href={href} className='border border-primary px-6 py-[10px] rounded-3xl hover:bg-primary transition-all ease-in'>
+    <a
+      href={href}
+      className={`border border-primary px-6 py-[10px] rounded-3xl hover:bg-primary transition-all ease-in ${className}`}
+      onClick={onClick}
+      aria-label={text}
+    >
       {text}
     </a>
   )
