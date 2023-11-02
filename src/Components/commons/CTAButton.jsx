73f2c5909ce88e6157694c7ccdee00cb05819bf3
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-02 20:46:36 +0100

    Update CTAButton styles

--- src/Components/commons/CTAButton.jsx
+++ src/Components/commons/CTAButton.jsx
@@ -2,7 +2,7 @@ const CTAButton = ({ href, text, className, onClick }) => {
   return (
     <a
       href={href}
-      className={`border border-primary px-6 py-[10px] rounded-3xl hover:bg-primary transition-all ease-in ${className}`}
+      className={`border hover:text-tag font-bold border-primary px-6 py-[10px] rounded-3xl hover:bg-primary transition-all ease-in ${className}`}
       onClick={onClick}
       aria-label={text}
     >
