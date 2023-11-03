cf4322700b7ea438a600a7f2856e5f4dd50e392d
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-03 20:48:51 +0100

    text hover change for logo

--- src/Components/home/Navbar/utils/Logo.jsx
+++ src/Components/home/Navbar/utils/Logo.jsx
@@ -1,13 +1,16 @@
 const Logo = () => {
   return (
-    <a href='/' className='flex items-center'>
+    <a
+      href='/'
+      className='flex items-center'
+    >
       <img
         loading='lazy'
         className='h-8 mr-3'
         alt='Logo'
         src='./assets/logo.svg'
       />
-      <span className='self-center text-2xl font-semibold whitespace-nowrap hover:text-primary transition-all ease-in'>
+      <span className='self-center text-xl md:text-2xl lg:text-3xl font-bold hover:text-transparent bg-clip-text bg-gradient-to-r from-primary via-[#FD7828] to-[#FA484F] transition-all ease-in'>
         DigitaRE
       </span>
     </a>
