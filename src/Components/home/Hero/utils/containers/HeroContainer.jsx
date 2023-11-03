62aa9fb92c0c3cf15af3b5c1647997a62d70a353
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-03 20:48:21 +0100

    "responsive" hero svg bg

--- src/Components/home/Hero/utils/containers/HeroContainer.jsx
+++ src/Components/home/Hero/utils/containers/HeroContainer.jsx
@@ -1,7 +1,7 @@
 const HeroContainer = ({ children }) => {
   return (
     <section
-      className='px-16 bg-[url(/assets/shaders.svg)] bg-no-repeat bg-cover'
+      className='px-16 bg-[url(/assets/shaders.svg)] bg-no-repeat bg-cover bg-center'
     >
       {children}
     </section>
