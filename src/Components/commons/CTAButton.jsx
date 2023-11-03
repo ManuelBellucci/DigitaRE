47609600bddfd26053a97a6bc8196a08b19fc2d6
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-03 20:47:51 +0100

    .

--- src/Components/commons/CTAButton.jsx
+++ src/Components/commons/CTAButton.jsx
@@ -2,7 +2,7 @@ const CTAButton = ({ href, text, className, onClick }) => {
   return (
     <a
       href={href}
-      className={`border hover:text-tag font-bold border-primary px-6 py-[10px] rounded-3xl hover:bg-primary transition-all ease-in ${className}`}
+      className={`border font-bold border-primary px-6 py-[10px] rounded-3xl hover:bg-primary hover:text-tag transition-all ease-in ${className}`}
       onClick={onClick}
       aria-label={text}
     >
