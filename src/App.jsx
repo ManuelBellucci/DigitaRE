b8ff7d1610c3a540f2bca6e81ddd828bd775ddca
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-13 16:48:54 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Hero from './Components/home/Hero/Hero.jsx'
+import Hero from './Components/home/Hero/Hero'
 import FirstSection from './Components/home/FirstSection/FirstSection'
 import SecondSection from './Components/home/SecondSection/SecondSection'
 import ThirdSection from './Components/home/ThirdSection/ThirdSection'
 import Reviews from './Components/home/Reviews/Reviews'
 import { Fade } from 'react-awesome-reveal'
 import { FloatButton } from 'antd'
-import Contact from './Components/home/Contact/Contact.jsx'
+import Contact from './Components/home/Contact/Contact'
 function App () {
   return (
     <>
