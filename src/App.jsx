5e1cf23dddab2db5cf012d6f764123be16e949a5
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-14 02:10:53 +0100

    Add react-router-dom to App.jsx for doing /blog

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,39 @@ import Reviews from './Components/home/Reviews/Reviews'
 import { Fade } from 'react-awesome-reveal'
 import { FloatButton } from 'antd'
 import Contact from './Components/home/Contact/Contact'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 function App () {
   return (
-    <>
-      <Hero />
-      <Fade triggerOnce>
-        <FirstSection />
-      </Fade>
-      <Fade triggerOnce>
-        <SecondSection />
-      </Fade>
-      <Fade triggerOnce>
-        <ThirdSection />
-      </Fade>
-      <Fade triggerOnce>
-        <Reviews />
-      </Fade>
-      <Fade triggerOnce>
-        <Contact />
-      </Fade>
+    <Router>
+      <Routes>
+        <Route path='/blog' element={<h1>Blog DigitaRE</h1>} />
+        <Route
+          path='/'
+          element={
+            <>
+              <Hero />
+              <Fade triggerOnce>
+                <FirstSection />
+              </Fade>
+              <Fade triggerOnce>
+                <SecondSection />
+              </Fade>
+              <Fade triggerOnce>
+                <ThirdSection />
+              </Fade>
+              <Fade triggerOnce>
+                <Reviews />
+              </Fade>
+              <Fade triggerOnce>
+                <Contact />
+              </Fade>
+            </>
+          }
+        />
+      </Routes>
       <FloatButton.BackTop className='hover:bg-myGray' />
-    </>
+    </Router>
   )
 }
 
