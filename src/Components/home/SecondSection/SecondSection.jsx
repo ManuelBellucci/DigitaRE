9a7d88d3682bc830a59fcc5a61400bde93bc232c
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-22 19:59:52 +0100

    Update SecondSection component with overflow-hidden and replace image format

--- src/Components/home/SecondSection/SecondSection.jsx
+++ src/Components/home/SecondSection/SecondSection.jsx
@@ -4,7 +4,7 @@ import data from '../../../data/secondsection.json'
 import { Slide } from 'react-awesome-reveal'
 
 const SecondSection = () => (
-  <section className='bg-black w-full h-min grid grid-rows-1 lg:grid-cols-2'>
+  <section className='bg-black w-full h-min grid grid-rows-1 lg:grid-cols-2 overflow-hidden'>
     <SecondSectionImage />
     <Slide triggerOnce direction='right' className='flex self-center'>
       <SecondSectionContentWrapper
--- src/Components/home/SecondSection/utils/SecondSectionImage.jsx
+++ src/Components/home/SecondSection/utils/SecondSectionImage.jsx
@@ -3,7 +3,7 @@ import { Slide } from 'react-awesome-reveal'
 const SecondSectionImage = () => (
   <div className='flex justify-center items-center h-max p-8 pb-0 lg:p-0 lg:h-[600px]'>
     <Slide triggerOnce>
-      <div className='bg-[url(/assets/persona.png)] bg-cover p-20 md:p-28 lg:p-52 border border-white rounded-full bg-no-repeat' />
+      <div className='bg-[url(/assets/persona.webp)] bg-cover p-20 md:p-28 lg:p-52 border border-white rounded-full bg-no-repeat' />
     </Slide>
   </div>
 )
