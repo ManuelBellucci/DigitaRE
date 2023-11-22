1013d34ab546f96da1e8e805a57d35086e2d4eae
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-22 19:57:25 +0100

    Add Footer component

---
+++ src/Components/home/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import CTAButtonWithLink from '../../commons/CTAWithLink'
+
+const Footer = () => {
+  return (
+    <footer className='w-full p-4 bg-[#101010] text-white'>
+      <CTAButtonWithLink href='/admin' text='Admin login' />
+    </footer>
+  )
+}
+export default Footer
