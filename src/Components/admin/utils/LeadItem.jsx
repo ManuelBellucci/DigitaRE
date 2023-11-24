9ff6b5ca6b7b5d56dd355b25c13064151b8f7c47
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-24 16:03:52 +0100

    Update LeadItem component styling

--- src/Components/admin/utils/LeadItem.jsx
+++ src/Components/admin/utils/LeadItem.jsx
@@ -1,8 +1,8 @@
 const LeadItem = ({ lead }) => {
   return (
-    <div className='grid grid-cols-3 text-black'>
-      <div className='text-myBlue text-lg font-bold'>{lead.name} {lead.surname}</div>
-      <div className='text-black text-sm'>{lead.email}</div>
+    <div className='grid grid-cols-3 text-black items-center'>
+      <div className='text-myBlue text-base lg:text-lg font-bold'>{lead.name} {lead.surname}</div>
+      <div className='text-black text-sm truncate'>{lead.email}</div>
       <div className='text-black text-sm'>{lead.phone}</div>
     </div>
   )
