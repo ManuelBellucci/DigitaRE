bad4ad8a9031199a030464e3b8cf58ae852faab1
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-11-24 16:02:18 +0100

    Update footer link in Footer component

--- src/Components/home/Footer/Footer.jsx
+++ src/Components/home/Footer/Footer.jsx
@@ -1,9 +1,14 @@
-import CTAWithLink from '../../commons/CTAWithLink'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <footer className='w-full p-4 bg-[#101010] text-white'>
-      <CTAWithLink href='/admin' text='Admin login' />
+      <Link
+        to='/admin'
+        className='inline-flex border font-bold border-primary px-6 py-[10px] rounded-md hover:bg-primary hover:text-tag transition-all ease-in'
+      >
+        <img src='/assets/lock.svg' height='20' width='20' alt='lock icon' className='h-4 w-4' />
+      </Link>
     </footer>
   )
 }
